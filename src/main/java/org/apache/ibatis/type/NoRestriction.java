a19ec768ff623aa639dc62c1ea04db47cd91b3b7
freekingdoms <freekingdoms@outlook.com>
2016-07-13 17:53:07 +0800

    add no strict mode
    v1

---
+++ src/main/java/org/apache/ibatis/type/NoRestriction.java
@@ -0,0 +1,12 @@
+package org.apache.ibatis.type;
+
+/**
+ * a blank interface for pojo or vo to add missed fields parameter
+ * before executing that
+ * 
+ * @author CK
+ *
+ */
+public interface NoRestriction {
+
+}
