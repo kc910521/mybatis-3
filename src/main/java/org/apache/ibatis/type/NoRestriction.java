99c49b7dcdc7b36954ca592c58fd1f307758f832
kc910521 <kc910521@qq.com>
2016-07-13 18:11:47 +0800

    here4fun

--- src/main/java/org/apache/ibatis/type/NoRestriction.java
+++ src/main/java/org/apache/ibatis/type/NoRestriction.java
@@ -4,9 +4,9 @@ package org.apache.ibatis.type;
  * a blank interface for pojo or vo to add missed fields parameter
  * before executing that
  * 
- * @author CK
+ * @author kc910521
  *
  */
 public interface NoRestriction {
-
+	//can be set fucntion here for more functions
 }
